f7507bebcebec7bd54b422d2837208aa4df85d92
Jeremy Tennant <tennantje@gmail.com>
2023-09-15 17:16:51 +1000

    enabled autloading custom packages

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
 --[[
 
 =====================================================================
@@ -221,7 +223,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+   { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
